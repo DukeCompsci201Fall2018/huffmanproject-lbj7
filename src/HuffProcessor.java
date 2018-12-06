cbd5730335bf25aae38ef64ee4153413cd45ac87
Louis Jensen <louis.jensen20@yahoo.com>
2018-12-06 02:33:11 -0500

    compress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -84,8 +84,8 @@ public class HuffProcessor {
 	}
 
 	private void codingHelper(HuffNode root, String path, String[] encodings) {
+		if (root == null) return;
 		 if (root.myRight == null && root.myLeft == null) {
-			 	if (root.myValue == PSEUDO_EOF) return;
 		        encodings[root.myValue] = path;
 		        return;
 		 } else {
